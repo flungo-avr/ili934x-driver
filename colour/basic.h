6cdb084b54b150ef8765ce9c5dfe8ade7f459b56
Fabrizio <fab@lungo.co.uk>
2015-05-03 12:27:54 +0100

    Basic colours.

---
+++ colour/basic.h
@@ -0,0 +1,25 @@
+/* Colour definitions RGB565 */
+#ifndef WHITE
+  #define WHITE       0xFFFF
+#endif
+#ifndef BLACK
+  #define BLACK       0x0000
+#endif
+#ifndef BLUE
+  #define BLUE        0x001F
+#endif
+#ifndef GREEN
+  #define GREEN       0x07E0
+#endif
+#ifndef BLACK
+  #define CYAN        0x07FF
+#endif
+#ifndef BLACK
+  #define RED         0xF800
+#endif
+#ifndef BLACK
+  #define MAGENTA     0xF81F
+#endif
+#ifndef BLACK
+  #define YELLOW      0xFFE0
+#endif
