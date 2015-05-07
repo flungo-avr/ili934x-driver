978a463cc1e59ddb4b23509995470fcfc3b79531
Fabrizio <fab@lungo.co.uk>
2015-05-07 21:37:15 +0100

    Updated commands to match documentation better.

--- ili934x.h
+++ ili934x.h
@@ -1,106 +1,127 @@
-/*  Author: Steve Gunn
- * Licence: This work is licensed under the Creative Commons Attribution License.
- *           View this license at http://creativecommons.org/about/licenses/
+/* The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Fabrizio Lungo <fab@lungo.co.uk>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
  */
 
-#define BLC       4
-#define RESET     7
-
-#define CMD_ADDR  0x4000
-#define DATA_ADDR 0x4100
+#include <stdbool.h>
+#include "ili934_conf.h"
 
 #define write_cmd(cmd) \
-  asm volatile("sts %0,%1" :: "i" (CMD_ADDR), "r" (cmd) : "memory");
+  asm volatile("sts %0,%1" :: "i" (LCD_CMD_ADDR), "r" (cmd) : "memory");
 #define write_data(data) \
-  asm volatile("sts %0,%1" :: "i" (DATA_ADDR), "r" (data) : "memory");
+  asm volatile("sts %0,%1" :: "i" (LCD_DATA_ADDR), "r" (data) : "memory");
 #define write_data16(data) \
-  asm volatile("sts %0,%B1 \n\t sts %0,%A1" :: "i" (DATA_ADDR), "r" (data)  : "memory");
+  asm volatile("sts %0,%B1 \n\t sts %0,%A1" :: "i" (LCD_DATA_ADDR), "r" (data)  : "memory");
 #define write_cmd_data(cmd, data) \
-  asm volatile("sts %0,%1 \n\t sts %2,%3" :: "i" (CMD_ADDR), "r" (cmd), "i" (DATA_ADDR), "r" (data)  : "memory");
+  asm volatile("sts %0,%1 \n\t sts %2,%3" :: "i" (LCD_CMD_ADDR), "r" (cmd), "i" (LCD_DATA_ADDR), "r" (data)  : "memory");
 
-/* Basic Commands */
-#define NO_OPERATION                              0x00
-#define SOFTWARE_RESET                            0x01
-#define READ_DISPLAY_IDENTIFICATION_INFORMATION   0x04
-#define READ_DISPLAY_STATUS                       0x09
-#define READ_DISPLAY_POWER_MODE                   0x0A
-#define READ_DISPLAY_MADCTL                       0x0B
-#define READ_DISPLAY_PIXEL_FORMAT                 0x0C
-#define READ_DISPLAY_IMAGE_FORMAT                 0x0D
-#define READ_DISPLAY_SIGNAL_MODE                  0x0E
-#define READ_DISPLAY_SELF_DIAGNOSTIC_RESULT       0x0F
-#define ENTER_SLEEP_MODE                          0x10
-#define SLEEP_OUT                                 0x11
-#define PARTIAL_MODE_ON                           0x12
-#define NORMAL_DISPLAY_MODE_ON                    0x13
-#define DISPLAY_INVERSION_OFF                     0x20
-#define DISPLAY_INVERSION_ON                      0x21
-#define GAMMA_SET                                 0x26
-#define DISPLAY_OFF                               0x28
-#define DISPLAY_ON                                0x29
-#define COLUMN_ADDRESS_SET                        0x2A
-#define PAGE_ADDRESS_SET                          0x2B
-#define MEMORY_WRITE                              0x2C
-#define COLOR_SET                                 0x2D
-#define MEMORY_READ                               0x2E
-#define PARTIAL_AREA                              0x30
-#define VERTICAL_SCROLLING_DEFINITION             0x33
-#define TEARING_EFFECT_LINE_OFF                   0x34
-#define TEARING_EFFECT_LINE_ON                    0x35
-#define MEMORY_ACCESS_CONTROL                     0x36
-#define VERTICAL_SCROLLING_START_ADDRESS          0x37
-#define IDLE_MODE_OFF                             0x38
-#define IDLE_MODE_ON                              0x39
-#define PIXEL_FORMAT_SET                          0x3A
-#define WRITE_MEMORY_CONTINUE                     0x3C
-#define READ_MEMORY_CONTINUE                      0x3E
-#define SET_TEAR_SCANLINE                         0x44
-#define GET_SCANLINE                              0x45
-#define WRITE_DISPLAY_BRIGHTNESS                  0x51
-#define READ_DISPLAY_BRIGHTNESS                   0x52
-#define WRITE_CTRL_DISPLAY                        0x53
-#define READ_CTRL_DISPLAY                         0x54
-#define WRITE_CONTENT_ADAPTIVE_BRIGHTNESS_CONTROL 0x55
-#define READ_CONTENT_ADAPTIVE_BRIGHTNESS_CONTROL  0x56
-#define WRITE_CABC_MINIMUM_BRIGHTNESS             0x5E
-#define READ_CABC_MINIMUM_BRIGHTNESS              0x5F
-#define READ_ID1                                  0xDA
-#define READ_ID2                                  0xDB
-#define READ_ID3                                  0xDC
+/* Level 1 Commands        HEX     Command Function */
+#define ILI934X_NOP        0x00 /* No Operation */
+#define ILI934X_SWRESET    0x01 /* Software Reset */
+#define ILI934X_RDDIDIF    0x04 /* Read Display Identification Information */
+#define ILI934X_RDDST      0x09 /* Read Display Status */
+#define ILI934X_RDDPM      0x0A /* Read Display Power Mode */
+#define ILI934X_RDDMADCTL  0x0B /* Read Display MADCTL */
+#define ILI934X_RDDCOLMOD  0x0C /* Read Display Pixel Format */
+#define ILI934X_RDDIM      0x0D /* Read Display Image Format */
+#define ILI934X_RDDSM      0x0E /* Read Display Signal Mode */
+#define ILI934X_RDDSDR     0x0F /* Read Display Self-Diagnostic Result */
+#define ILI934X_SLPIN      0x10 /* Enter Sleep Mode */
+#define ILI934X_SLPOUT     0x11 /* Sleep OUT */
+#define ILI934X_PTLON      0x12 /* Partial Mode ON */
+#define ILI934X_NORON      0x13 /* Normal Display Mode ON */
+#define ILI934X_INVOFF     0x20 /* Display Inversion OFF */
+#define ILI934X_INVON      0x21 /* Display Inversion ON */
+#define ILI934X_GAMSET     0x26 /* Gamma Set */
+#define ILI934X_DISPOFF    0x28 /* Display OFF */
+#define ILI934X_DISPON     0x29 /* Display ON */
+#define ILI934X_CASET      0x2A /* Column Address Set */
+#define ILI934X_PASET      0x2B /* Page Address Set */
+#define ILI934X_RAMWR      0x2C /* Memory Write */
+#define ILI934X_RGBSET     0x2D /* Color SET */
+#define ILI934X_RAMRD      0x2E /* Memory Read */
+#define ILI934X_PTLAR      0x30 /* Partial Area */
+#define ILI934X_VSCRDEF    0x33 /* Vertical Scrolling Definition */
+#define ILI934X_TEOFF      0x34 /* Tearing Effect Line OFF */
+#define ILI934X_TEON       0x35 /* Tearing Effect Line ON */
+#define ILI934X_MADCTR     0x36 /* Memory Access Control */
+#define ILI934X_VSCRSADD   0x37 /* Vertical Scrolling Start Address */
+#define ILI934X_IDMOFF     0x38 /* Idle Mode OFF */
+#define ILI934X_IDMON      0x39 /* Idle Mode ON */
+#define ILI934X_COLMOD     0x3A /* Pixel Format Set */
+#define ILI934X_RAMWRCNT   0x3C /* Write Memory Continue */
+#define ILI934X_RAMRDCNT   0x3E /* Read Memory Continue */
+#define ILI934X_TSSET      0x44 /* Set Tear Scanline */
+#define ILI934X_TSGET      0x45 /* Get Scanline */
+#define ILI934X_WRDISBV    0x51 /* Write Display Brightness */
+#define ILI934X_RDDISBV    0x52 /* Read Display Brightness */
+#define ILI934X_WRCTRLD    0x53 /* Write CTRL Display */
+#define ILI934X_RDCTRLD    0x54 /* Read CTRL Display */
+#define ILI934X_WRCABC     0x55 /* Write Content Adaptive Brightness Control */
+#define ILI934X_RDCABC     0x56 /* Read Content Adaptive Brightness Control */
+#define ILI934X_WRCABCMIN  0x5E /* Write CABC Minimum Brightness */
+#define ILI934X_RDCABCMIN  0x5F /* READ CABC Minimum Brightness */
+#define ILI934X_RDID1      0xDA /* Read ID1 */
+#define ILI934X_RDID2      0xDB /* Read ID2 */
+#define ILI934X_RDID3      0xDC /* Read ID3 */
 
-/* Extended Commands */
-#define RGB_INTERFACE_SIGNAL_CONTROL              0xB0
-#define FRAME_CONTROL_IN_NORMAL_MODE              0xB1
-#define FRAME_CONTROL_IN_IDLE_MODE                0xB2
-#define FRAME_CONTROL_IN_PARTIAL_MODE             0xB3
-#define DISPLAY_INVERSION_CONTROL                 0xB4
-#define BLANKING_PORCH_CONTROL                    0xB5
-#define DISPLAY_FUNCTION_CONTROL                  0xB6
-#define ENTRY_MODE_SET                            0xB7
-#define BACKLIGHT_CONTROL_1                       0xB8
-#define BACKLIGHT_CONTROL_2                       0xB9
-#define BACKLIGHT_CONTROL_3                       0xBA
-#define BACKLIGHT_CONTROL_4                       0xBB
-#define BACKLIGHT_CONTROL_5                       0xBC
-#define BACKLIGHT_CONTROL_7                       0xBE
-#define BACKLIGHT_CONTROL_8                       0xBF
-#define POWER_CONTROL_1                           0xC0
-#define POWER_CONTROL_2                           0xC1
-#define POWER_CONTROL3_(FOR_NORMAL_MODE)          0xC2
-#define POWER_CONTROL4_(FOR_IDLE_MODE)            0xC3
-#define POWER_CONTROL5_(FOR_PARTIAL_MODE)         0xC4
-#define VCOM_CONTROL_1                            0xC5
-#define VCOM_CONTROL_2                            0xC7
-#define NV_MEMORY_WRITE                           0xD0
-#define NV_MEMORY_PROTECTION_KEY                  0xD1
-#define NV_MEMORY_STATUS_READ                     0xD2
-#define READ_ID4                                  0xD3
-#define POSITIVE_GAMM_CORRECTION                  0xE0
-#define NEGATIVE_GAMMA_CORRECTION                 0xE1
-#define DIGITAL_GAMMA_CONTROL                     0xE2
-#define DIGITAL_GAMMA_CONTROL2                    0xE3
-#define INTERFACE_CONTROL                         0xF6
+/* Level 2 Commands        HEX     Command Function */
+#define ILI934X_IFMODE     0xB0 /* RGB Interface Signal Control */
+#define ILI934X_FRMCTR1    0xB1 /* Frame Control (In Normal Mode) */
+#define ILI934X_FRMCTR2    0xB2 /* Frame Control (In Idle Mode) */
+#define ILI934X_FRMCTR3    0xB3 /* Frame Control (In Partial Mode) */
+#define ILI934X_INVTR      0xB4 /* Display Inversion Control */
+#define ILI934X_PRCTR      0xB5 /* Blanking Porch Control */
+#define ILI934X_DISCTRL    0xB6 /* Display Function Control */
+#define ILI934X_ETMOD      0xB7 /* Entry Mode Set */
+#define ILI934X_BLCTRL1    0xB8 /* Backlight Control 1 */
+#define ILI934X_BLCTRL2    0xB9 /* Backlight Control 2 */
+#define ILI934X_BLCTRL3    0xBA /* Backlight Control 3 */
+#define ILI934X_BLCTRL4    0xBB /* Backlight Control 4 */
+#define ILI934X_BLCTRL5    0xBC /* Backlight Control 5 */
+#define ILI934X_BLCTRL7    0xBE /* Backlight Control 7 */
+#define ILI934X_BLCTRL8    0xBF /* Backlight Control 8 */
+#define ILI934X_PWCTRL1    0xC0 /* Power Control 1 */
+#define ILI934X_PWCTRL2    0xC1 /* Power Control 2 */
+#define ILI934X_PWCTRL3    0xC2 /* Power Control 3 */
+#define ILI934X_PWCTRL4    0xC3 /* Power Control 4 */
+#define ILI934X_PWCTRL5    0xC4 /* Power Control 5 */
+#define ILI934X_VMCTRL1    0xC5 /* VCOM Control 1 */
+#define ILI934X_VMCTRL2    0xC7 /* VCOM Control 2 */
+#define ILI934X_NVMWR      0xD0 /* NV Memory Write */
+#define ILI934X_NVMPKEY    0xD1 /* NV Memory Protection Key */
+#define ILI934X_RDNVM      0xD2 /* NV Memory Status Read */
+#define ILI934X_RDID4      0xD3 /* Read ID4 */
+#define ILI934X_PGAMCTRL   0xE0 /* Positive Gamma Correction */
+#define ILI934X_NGAMCTRL   0xE1 /* Negative Gamma Correction */
+#define ILI934X_DGAMCTRL1  0xE2 /* Digital Gamma Correction 1 */
+#define ILI934X_DGAMCTRL2  0xE3 /* Digital Gamma Correction 2 */
+#define ILI934X_IFCTL      0xF6 /* Interface Control */
 
-/* Undocumented commands */
-#define INTERNAL_IC_SETTING                       0xCB
-#define GAMMA_DISABLE                             0xF2
+/* Extended Register Commands */
+#define ILI934X_PWCTRLA    0xCB /* Power Control A */
+#define ILI934X_PWCTRLB    0xCF /* Power Control B */
+#define ILI934X_DRTCTRLA   0xE8 /* Driver Timing Control A */
+#define ILI934X_DRTCTRLB   0xEA /* Driver Timing Control B */
+#define ILI934X_PWOSC      0xED /* Power on sequence control */
+#define ILI934X_EN3G       0xF2 /* Enable 3 Gamma Control */
+#define ILI934X_PRCTRL     0xF7 /* Pump Rario Control */
