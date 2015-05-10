b63863f78ecdb83cb10af82835284074d18de9ad
Fabrizio <fab@lungo.co.uk>
2015-05-10 14:08:51 +0100

    Generated from e0f495eee79bd1d527d2717ff2fd0952b552d413

---
+++ search/all_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['lcd_5fdisplay',['lcd_display',['../structlcd__display.html',1,'']]],
+  ['lcd_5fpoint',['lcd_point',['../structlcd__point.html',1,'']]],
+  ['lcd_5fregion',['lcd_region',['../structlcd__region.html',1,'']]]
+];
---
+++ search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "l",
+  1: "l"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Data Structures"
+};
+
